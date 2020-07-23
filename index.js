0c1ec99ef6327798d654c8834fc35feaefe55c6c
Nelson Carabali <ncarabali@intergrupo.com>
2020-07-23 11:41:56 -0500

    ADD: Adding element in page home

---
+++ index.js
@@ -0,0 +1,11 @@
+const container = require('./src/startup/container');
+const server = container.resolve("app");
+const { MONGO_URI } = container.resolve('config');
+
+
+const mongoose = require('mongoose');
+
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(server.start())
+    .catch(console.log)
